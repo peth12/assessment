69962986ceef3e0d48d41fded10cf1bb9caa4d17
peth12 <pethkk12@gmail.com>
2024-02-26 20:00:29 +0700

    create userticket repository and userticket service

--- src/main/java/com/example/ticket/userticket/repository/UserTicketRepository.java
+++ src/main/java/com/example/ticket/userticket/repository/UserTicketRepository.java
@@ -1,2 +1,8 @@
-package com.example.ticket.userticket.repository;public class UserTicketRepository {
+package com.example.ticket.userticket.repository;
+
+import com.example.ticket.userticket.model.UserTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTicketRepository extends JpaRepository<UserTicket, Long> {
+
 }
--- src/main/java/com/example/ticket/userticket/service/UserTicketService.java
+++ src/main/java/com/example/ticket/userticket/service/UserTicketService.java
@@ -1,2 +1,36 @@
-package com.example.ticket.userticket.service;public class UserTicketService {
+package com.example.ticket.userticket.service;
+
+
+import com.example.ticket.exception.NotFoundException;
+import com.example.ticket.lottery.model.Lottery;
+import com.example.ticket.lottery.repository.LotteryRepository;
+import com.example.ticket.userticket.model.UserTicket;
+import com.example.ticket.userticket.repository.UserTicketRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserTicketService {
+
+    private final UserTicketRepository userTicketRepository;
+    private final LotteryRepository lotteryRepository;
+
+
+    public UserTicketService(UserTicketRepository userTicketRepository, LotteryRepository lotteryRepository) {
+        this.userTicketRepository = userTicketRepository;
+        this.lotteryRepository = lotteryRepository;
+    }
+
+    public Long buyLottery(String userId, String ticketId) {
+        UserTicket userTicket = new UserTicket();
+
+        Optional<Lottery> optionalLottery = lotteryRepository.findById(ticketId);
+        Lottery lottery = new Lottery();
+
+        if(optionalLottery.isEmpty()){
+            throw new NotFoundException("This ticket number was not found.");
+        }
+
+    }
 }
