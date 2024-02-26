f23784bfa1834f17f466ed77d04ff77a35272569
peth12 <pethkk12@gmail.com>
2024-02-26 11:58:08 +0700

    create userTicket model

--- src/main/java/com/example/ticket/userticket/model/UserTicket.java
+++ src/main/java/com/example/ticket/userticket/model/UserTicket.java
@@ -1,2 +1,27 @@
-package com.example.ticket.userticket.model;public class UserTicket {
+package com.example.ticket.userticket.model;
+
+
+import com.example.ticket.lottery.model.Lottery;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_ticket")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTicket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "ticket_id" , referencedColumnName = "ticket_id")
+    private Lottery lottery;
 }
